20fae6002684b79eaa4e381ee28ac8804b7f99b8
Sam Wynne <sam.wynne@snapraise.com>
2022-12-16 23:06:27 -0600

    Fixed header

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ function Header({}: Props) {
   return (
     <div>
 
-    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+    <header className="fixed top-0 left-0 right-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div 
       initial={{
         x: -500,
