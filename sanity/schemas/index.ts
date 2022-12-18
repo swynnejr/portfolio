5cc88b2151254e7cf49ab142b3d2071a878d8e12
Sam Wynne <sam.wynne@snapraise.com>
2022-12-17 18:12:52 -0600

    Defining 'portfolioInfo' schema for Sanity

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -2,5 +2,6 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import portfolioInfo from './portfolioInfo'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, blockContent, portfolioInfo]
