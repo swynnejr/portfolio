5ecf66ca14ba761c8b1a5efa126d5c65f402c976
Sam Wynne <sam.wynne@snapraise.com>
2022-12-20 17:32:36 -0600

    Reformatted experience cards to pull from Sanity

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -29,52 +29,32 @@ function ExperienceCard({experience}: Props) {
           src={urlFor(experience?.companyImage).url()}
           width={100}
           height={100}
-          alt="Picture of Sam"
+          alt="Company Logo"
         />
       </motion.div>
       <div className="px-0 md:px-10">
-        <h4 className="text-4xl font-light">Technical Product Manager</h4>
-        <p className="font-bold text-2xl mt-1">Bazari</p>
+        <h4 className="text-4xl font-light">{experience?.jobTitle}</h4>
+        <p className="font-bold text-2xl mt-1">{experience?.company}</p>
         <div className="flex space-x-2 my-2">
           {/* Tech Used */}
-          <Image
-          className="rounded-full"
-          src="https://brandeps.com/logo-download/R/React-logo-vector-01.svg"
-          width={50}
-          height={50}
-          alt="React logo"
-        />
-                  <Image
-          className="rounded-full"
-          src="https://brandeps.com/logo-download/R/React-logo-vector-01.svg"
-          width={50}
-          height={50}
-          alt="React logo"
-        />
-                  <Image
-          className="rounded-full"
-          src="https://brandeps.com/logo-download/R/React-logo-vector-01.svg"
-          width={50}
-          height={50}
-          alt="React logo"
-        />
-                  <Image
-          className="rounded-full"
-          src="https://brandeps.com/logo-download/R/React-logo-vector-01.svg"
-          width={50}
-          height={50}
-          alt="React logo"
-        />
+          {experience.technologies?.map((technology) => (
+            <Image
+            key={technology._id}
+            className="h-10 w-10 rounded-full"
+            src={urlFor(technology.image).url()}
+            width={50}
+            height={50}
+            alt="Technology logo"
+          />
+
+          ))}
         </div>
-        <p className="uppercase py-5 text-gray-300">April 2022 - December 2022
+        <p className="uppercase py-5 text-gray-300">{new Date(experience.dateStarted).toDateString()} -{" "} {experience.isCurrentlyWorkingHere ? "Present" : new Date(experience.dateEnded).toDateString()}
 </p>
 <ul className="list-disc space-y-4 ml-5 text-lg">
-  <li>Refined the product vision with CEO/CTO based off my competitor UX research and user interviews</li>
-  <li>Fostered trust and rapport with leaders of all departments to consistently achieve cross-functional buy-in </li>
-  <li>Simultaneously managed remote developers in Brazil, Lebanon, Switzerland, and USA</li>
-  <li>Scrum Master & Product Owner, driving the product vision/roadmap and employee leadership</li>
-  <li>Implemented Agile Methodology with daily standups, biweekly sprints & grooming sessions, and a major pivot</li>
-  <li>Researched available APIs (some without documentation) for our site to utilize six data sources</li>
+  {experience.points?.map((point, i) => (
+    <li key={i}>{point}</li>
+  ))}
 </ul>
       </div>
     </article>
