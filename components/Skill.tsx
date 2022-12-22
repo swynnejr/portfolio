5f8a65000a3f9091cbe6ae619128ff57128c0eb4
Sam Wynne <sam.wynne@snapraise.com>
2022-12-21 22:57:39 -0600

    Skills pull from Sanity with updated styling

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { Skill } from "../typings";
+import { urlFor } from "../sanity";
 
 type Props = {
+  skill: Skill;
   directionLeft?: boolean;
 };
 
-function Skill({ directionLeft }: Props) {
+function Skill({ skill, directionLeft }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
       <motion.div
         initial={{
-          x: directionLeft ? -200 : 200,
+          x: directionLeft ? -100 : 100,
+          y: directionLeft ? 100 : -100,
           opacity: 0,
         }}
         transition={{
@@ -19,20 +23,22 @@ function Skill({ directionLeft }: Props) {
         }}
         whileInView={{
           x: 0,
+          y: 0,
           opacity: 1,
         }}
       >
         <Image
-          className="rounded-full border border-gray-500 object-cover h-24 w-24 md:h-28 md:w-28 xl:h-32 xl:w-32 filter group-hover:grayscale transition duration-500 ease-in-out"
-          src="https://brandeps.com/logo-download/R/React-logo-vector-01.svg"
-          width={50}
-          height={50}
-          alt="Picture of Sam"
+          className="rounded-full border border-gray-500 object-cover p-1 h-24 w-24 md:h-28 md:w-28 xl:h-32 xl:w-32 filter group-hover:grayscale transition duration-500 ease-in-out"
+          src={urlFor(skill?.image).url()}
+          width={30}
+          height={30}
+          alt="Technology Logo"
         />
       </motion.div>
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-500 ease-in-out group-hover:bg-white h-24 w-24 md:h-28 md:w-28 xl:h-32 xl:w-32 rounded-full z-0">
-        <div className="flex items-center justify-center h-full">
-          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-500 ease-in-out group-hover:bg-white h-24 w-24 md:h-28 md:w-28 xl:h-32 xl:w-32 rounded-full z-10 border-4 border-black">
+        <div className="p-5 items-center justify-center h-full">
+          <p className="text-xl font-bold text-black opacity-100">{skill?.title}</p>
+          <p className="text-3xl font-bold text-black opacity-100">{skill?.progress}%</p>
         </div>
       </div>
     </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Skill from "./Skill";
+import { Skill as ISkill } from "../typings";
 
-type Props = {};
+type Props = {
+  skills: ISkill[];
+};
 
-function Skills({}: Props) {
+function Skills({skills}: Props) {
   return (
     <motion.div
     initial={{
@@ -24,22 +27,12 @@ function Skills({}: Props) {
         Hover over a skill for current proficiency
       </h3>
       <div className="grid grid-cols-4 gap-5">
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+        {skills?.slice(0, skills.length /2 ).map((skill) => (
+          <Skill key={skill._id} skill={skill} />
+        ))}
+        {skills?.slice(skills.length /2, skills.length ).map((skill) => (
+          <Skill key={skill._id} skill={skill} directionLeft />
+        ))}
       </div>
     </motion.div>
   );
