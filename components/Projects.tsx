40955be347fc0474b0a3a301228df7745c78283e
Sam Wynne <sam.wynne@snapraise.com>
2022-12-22 00:07:13 -0600

    tidying up .map

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -13,9 +13,9 @@ function Projects({ projects }: Props) {
     <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
       <h3 className="sectionTitle">Projects</h3>
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track gray-400/20 scrollbar-thumb-[#e93232]/50">
-        {projects.map((project, i) => (
+        {projects?.map((project, i) => (
           <div
-            key={i}
+            key={project._id}
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
           >
             <motion.div
