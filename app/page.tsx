e43391102f583b9c7064cfb845fc6f8def74c71f
Sam Wynne <sam.wynne@snapraise.com>
2022-12-31 19:08:13 -0600

    Ppgrading next13 from pages to app

---
+++ app/page.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import HomePage from './HomePage';
+import { Experience, PortfolioInfo, Project, Skill, Social } from "../typings";
+
+async function fetchPortfolioInfo() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPortfolioInfo`);
+
+  const data = await res.json();
+  const portfolioInfo: PortfolioInfo = data.portfolioInfo;
+
+  return portfolioInfo;
+}
+
+async function fetchExperiences() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`);
+
+  const data = await res.json();
+  const experience: Experience[] = data.experience;
+
+  return experience;
+}
+
+async function fetchProjects() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`);
+
+  const data = await res.json();
+  const projects: Project[] = data.projects;
+
+  return projects;
+}
+
+async function fetchSkills() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
+
+  const data = await res.json();
+  const skills: Skill[] = data.skills;
+
+  return skills;
+}
+async function fetchSocials() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
+
+  const data = await res.json();
+  const socials: Social[] = data.socials;
+
+  return socials;
+}
+
+type Props = {
+  portfolioInfo: PortfolioInfo;
+  experience: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[];
+}
+
+export default async function Page({}: Props) {
+  const portfolioInfo = await fetchPortfolioInfo();
+  const experience = await fetchExperiences();
+  const projects = await fetchProjects();
+  const skills = await fetchSkills();
+  const socials = await fetchSocials();
+  return (
+    <div>
+      <HomePage 
+        portfolioInfo={portfolioInfo} 
+        experience={experience}
+        projects={projects}
+        skills={skills}
+        socials={socials}
+      />
+    </div>
+  )
+}
+
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true
+  },
   images: {
     domains: ['yt3.googleusercontent.com', 'media-exp1.licdn.com', 'avatars.githubusercontent.com', 'brandeps.com', 'cdn.sanity.io'],
   },
