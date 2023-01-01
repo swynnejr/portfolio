270bb86e4a28fa201ec593bad321835c4e084baf
Sam Wynne <sam.wynne@snapraise.com>
2023-01-01 10:05:01 -0600

    removing defineCliConfig from sanity

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+// import {defineCliConfig} from 'sanity/cli'
 
-export default defineCliConfig({
-  api: {
-    projectId: 'ippijs42',
-    dataset: 'production'
-  }
-})
+// export default defineCliConfig({
+//   api: {
+//     projectId: 'ippijs42',
+//     dataset: 'production'
+//   }
+// })
