e16073fcdb72f1679ac699badfa6351121d6368c
Sam Wynne <sam.wynne@snapraise.com>
2023-01-02 17:22:31 -0600

    Forcing deployment by skipping api calls

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -2,65 +2,65 @@ import React from 'react'
 import HomePage from '../HomePage';
 import { Experience, PortfolioInfo, Project, Skill, Social } from "../../typings";
 
-async function fetchPortfolioInfo() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPortfolioInfo`, { next: { revalidate: 300 } });
+// async function fetchPortfolioInfo() {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPortfolioInfo`, { next: { revalidate: 300 } });
 
-  const data = await res.json();
-  const portfolioInfo: PortfolioInfo = data.portfolioInfo;
+//   const data = await res.json();
+//   const portfolioInfo: PortfolioInfo = data.portfolioInfo;
 
-  return portfolioInfo;
-}
+//   return portfolioInfo;
+// }
 
-async function fetchExperiences() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`, { next: { revalidate: 300 } });
+// async function fetchExperiences() {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`, { next: { revalidate: 300 } });
 
-  const data = await res.json();
-  const experience: Experience[] = data.experience;
+//   const data = await res.json();
+//   const experience: Experience[] = data.experience;
 
-  return experience;
-}
+//   return experience;
+// }
 
-async function fetchProjects() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`, { next: { revalidate: 300 } });
+// async function fetchProjects() {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`, { next: { revalidate: 300 } });
 
-  const data = await res.json();
-  const projects: Project[] = data.projects;
+//   const data = await res.json();
+//   const projects: Project[] = data.projects;
 
-  return projects;
-}
+//   return projects;
+// }
 
-async function fetchSkills() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`, { next: { revalidate: 300 } });
+// async function fetchSkills() {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`, { next: { revalidate: 300 } });
 
-  const data = await res.json();
-  const skills: Skill[] = data.skills;
+//   const data = await res.json();
+//   const skills: Skill[] = data.skills;
 
-  return skills;
-}
-async function fetchSocials() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`, { next: { revalidate: 300 } });
+//   return skills;
+// }
+// async function fetchSocials() {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`, { next: { revalidate: 300 } });
 
-  const data = await res.json();
-  const socials: Social[] = data.socials;
+//   const data = await res.json();
+//   const socials: Social[] = data.socials;
 
-  return socials;
-}
+//   return socials;
+// }
 
 export default async function Page() {
-  const portfolioInfo = await fetchPortfolioInfo();
-  const experience = await fetchExperiences();
-  const projects = await fetchProjects();
-  const skills = await fetchSkills();
-  const socials = await fetchSocials();
+  // const portfolioInfo = await fetchPortfolioInfo();
+  // const experience = await fetchExperiences();
+  // const projects = await fetchProjects();
+  // const skills = await fetchSkills();
+  // const socials = await fetchSocials();
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0  scrollbar-thin scrollbar-track gray-400/20 scrollbar-thumb-[#e93232]/50">
-      <HomePage 
+      {/* <HomePage 
         portfolioInfo={portfolioInfo} 
         experience={experience}
         projects={projects}
         skills={skills}
         socials={socials}
-      />
+      /> */}
     </div>
   )
 }
