1500430aa55078d734c19f838cf1d8700fa82818
Sam Wynne <sam.wynne@snapraise.com>
2023-01-08 18:52:42 -0600

    No changes to code, only format

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import HomePage from '../HomePage';
-import { Experience, PortfolioInfo, Project, Skill, Social } from "../../typings";
+import React from "react";
+import HomePage from "../HomePage";
+import {
+  Experience,
+  PortfolioInfo,
+  Project,
+  Skill,
+  Social,
+} from "../../typings";
 
 async function fetchPortfolioInfo() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPortfolioInfo`, { next: { revalidate: 300 } });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPortfolioInfo`,
+    { next: { revalidate: 300 } }
+  );
 
   const data = await res.json();
   const portfolioInfo: PortfolioInfo = data.portfolioInfo;
@@ -12,7 +21,10 @@ async function fetchPortfolioInfo() {
 }
 
 async function fetchExperiences() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`, { next: { revalidate: 300 } });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`,
+    { next: { revalidate: 300 } }
+  );
 
   const data = await res.json();
   const experience: Experience[] = data.experience;
@@ -21,7 +33,10 @@ async function fetchExperiences() {
 }
 
 async function fetchProjects() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`, { next: { revalidate: 300 } });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`,
+    { next: { revalidate: 300 } }
+  );
 
   const data = await res.json();
   const projects: Project[] = data.projects;
@@ -30,7 +45,9 @@ async function fetchProjects() {
 }
 
 async function fetchSkills() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`, { next: { revalidate: 300 } });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`, {
+    next: { revalidate: 300 },
+  });
 
   const data = await res.json();
   const skills: Skill[] = data.skills;
@@ -38,7 +55,10 @@ async function fetchSkills() {
   return skills;
 }
 async function fetchSocials() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`, { next: { revalidate: 300 } });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`,
+    { next: { revalidate: 300 } }
+  );
 
   const data = await res.json();
   const socials: Social[] = data.socials;
@@ -56,14 +76,13 @@ export default async function Page() {
   const socials = await fetchSocials();
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0  scrollbar-thin scrollbar-track gray-400/20 scrollbar-thumb-[#e93232]/50">
-      <HomePage 
-        portfolioInfo={portfolioInfo} 
+      <HomePage
+        portfolioInfo={portfolioInfo}
         experience={experience}
         projects={projects}
         skills={skills}
         socials={socials}
       />
     </div>
-  )
+  );
 }
-
