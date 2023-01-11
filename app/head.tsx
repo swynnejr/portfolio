12762c8851e33f28440655b40c577aacee328025
Sam Wynne <sam.wynne@snapraise.com>
2023-01-11 13:46:10 -0600

    undoing google adsense crossOrigin change

--- app/head.tsx
+++ app/head.tsx
@@ -7,7 +7,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
       <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9874753329084187"
-     crossorigin="anonymous"></Script>
+     crossOrigin="anonymous"></Script>
     </>
   )
 }
