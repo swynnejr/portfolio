ceacaa0113aa763f1da207aaa95d4106b8590470
Sam Wynne <sam.wynne@snapraise.com>
2023-01-11 15:40:11 -0600

    Google Analytics with sketchy Next 13 strategy

--- app/head.tsx
+++ app/head.tsx
@@ -7,7 +7,19 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
       <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9874753329084187"
-     crossOrigin="anonymous"></Script>
+      crossOrigin="anonymous"></Script>
+      <Script async strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-R406Y6H8W5" />
+      <Script id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+          gtag('config', 'G-R406Y6H8W5', {
+            page_path: window.location.pathname,
+            });
+          `,
+      }}
+      />
     </>
   )
 }
