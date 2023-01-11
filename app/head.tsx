32b45f1131f517d3904d806a3823032ba43c5e31
Sam Wynne <sam.wynne@snapraise.com>
2023-01-11 13:07:41 -0600

    HTML <script> to Next/script <Script> for google

--- app/head.tsx
+++ app/head.tsx
@@ -1,11 +1,13 @@
+import Script from "next/script";
+
 export default function Head() {
   return (
     <>
       <title>Sam Wynne</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9874753329084187"
-     crossOrigin="anonymous"></script>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9874753329084187"
+     crossorigin="anonymous"></Script>
     </>
   )
 }
