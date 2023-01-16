9b3f81353d58abdd55f15459995d8375a0fa6c05
Sam Wynne <saucersam@gmail.com>
2023-01-16 11:23:58 -0600

    Removed buggy Google AdSense snippet

--- app/head.tsx
+++ app/head.tsx
@@ -6,8 +6,6 @@ export default function Head() {
       <title>Sam Wynne</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
-      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9874753329084187"
-      crossOrigin="anonymous"></Script>
       <Script async strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-R406Y6H8W5" />
       <Script id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{
         __html: `
