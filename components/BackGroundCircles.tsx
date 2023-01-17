ea396308a3364e3ba57063ae2e425e72ff5303e3
Sam Wynne <saucersam@gmail.com>
2023-01-16 21:32:16 -0600

    Atom animation updates

--- components/BackGroundCircles.tsx
+++ components/BackGroundCircles.tsx
@@ -13,22 +13,22 @@ function BackGroundCircles({ portfolioInfo }: Props) {
       id="atom"
       className="h-[5em] w-[5em] lg:h-[10em] lg:w-[10-em] relative flex"
     >
-      <motion.div 
-      className="col-start-1 row-start-1 items-center my-auto mx-auto z-50"
-      initial={{ scale: 0.7 }}
-      animate={{ scale: 1 }}
-      transition={{
-        default: {
-          duration: 0.3,
-          ease: [0, 0.71, 0.2, 1.01]
-        },
-        scale: {
-          type: "spring",
-          damping: 5,
-          stiffness: 50,
-          restDelta: 0.001
-        }
-      }}
+      <motion.div
+        className="col-start-1 row-start-1 items-center my-auto mx-auto z-50"
+        initial={{ scale: 0.5, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{
+          default: {
+            duration: 0.3,
+            ease: [0, 0.71, 0.2, 1.01],
+          },
+          scale: {
+            type: "spring",
+            damping: 5,
+            stiffness: 50,
+            restDelta: 0.001,
+          },
+        }}
       >
         <Image
           className="mx-auto rounded-full object-cover h-[250px] w-[250px] md:h-[325px] md:w-[325px] lg:h-[400px] lg:w-[400px] xl:h-[500px] xl:w-[500px]"
@@ -38,18 +38,12 @@ function BackGroundCircles({ portfolioInfo }: Props) {
           alt="Picture of Sam"
         />
       </motion.div>
-      <motion.div 
-      className="right orbit"
-      initial={{scale: 0.01}}
-      animate={{scale: 1}}
-      transition={{duration: 4, delay: 2}}
-      ></motion.div>
-      <motion.div 
-      className="left orbit"
-      initial={{scale: 0.01}}
-      animate={{scale: 1}}
-      transition={{duration: 5, delay: 2}}
-      ></motion.div>
+      <div
+        className="right orbit"
+      ></div>
+      <div
+        className="left orbit"
+      ></div>
     </div>
   );
 }
